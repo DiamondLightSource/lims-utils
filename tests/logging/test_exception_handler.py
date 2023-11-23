049306345364e10fcd8e32ac61d28ad3aa110276
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2023-11-23 11:34:45 +0000

    Add tests

---
+++ tests/logging/test_exception_handler.py
@@ -0,0 +1,36 @@
+from expeye_utils.logging import log_exception_handler
+from fastapi import Request, HTTPException, status
+import pytest
+
+default_scope = {
+    "type": "http",
+    "state": {"user": "username"},
+    "path": "http://test.ac.uk",
+    "headers": {},
+}
+
+
+@pytest.mark.asyncio
+async def test_log_format(caplog):
+    """Should log message with user provided"""
+    request = Request(scope=default_scope)
+
+    await log_exception_handler(
+        request,
+        HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Failure details"),
+    )
+
+    assert "username @ http://test.ac.uk: Failure details" in caplog.text
+
+
+@pytest.mark.asyncio
+async def test_log_format_no_user(caplog):
+    """Should log message with no user provided"""
+    request = Request(scope={**default_scope, "state": {}})
+
+    await log_exception_handler(
+        request,
+        HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Failure details"),
+    )
+
+    assert "Unknown user @ http://test.ac.uk: Failure details" in caplog.text
