79f9776ad344eac4683934c8baf71b16f049565a
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-02-15 11:39:30 +0000

    Add test for nested JSON settings

--- tests/settings/test_settings.py
+++ tests/settings/test_settings.py
@@ -1,12 +1,21 @@
 from unittest.mock import patch
 
 import pytest
+from pydantic import BaseModel
 
-from lims_utils.settings import Settings
+from lims_utils.settings import Auth, Settings
 
 
 def new_read_all(_, _1):
     return ""
+
+
+class Nested(BaseModel):
+    nested: Auth
+
+
+class ExpandedSettings(Settings):
+    nested: Nested
 
 
 @pytest.mark.asyncio
@@ -34,5 +43,23 @@ async def test_custom(mock_json_loads):
     settings = Settings()
 
     assert settings.auth.endpoint == "https://localhost/diff-auth"
+
+    assert settings.db.pool == 90
+
+
+@pytest.mark.asyncio
+@patch("lims_utils.settings.json.loads")
+@patch("lims_utils.settings.Path.read_text", new=new_read_all)
+async def test_nested(mock_json_loads):
+    """Should return custom values"""
+    mock_json_loads.return_value = {
+        "auth": {"endpoint": "https://localhost/diff-auth"},
+        "db": {"pool": 90},
+        "nested": {"nested": {"endpoint": "https://localhost/diff-auth"}},
+    }
+
+    settings = ExpandedSettings()
+
+    assert settings.nested.nested.endpoint == "https://localhost/diff-auth"
 
     assert settings.db.pool == 90
