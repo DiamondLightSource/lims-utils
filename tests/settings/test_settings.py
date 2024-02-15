e2055c5d1425ef959c7b9b939efda3d52e6a77e5
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-02-15 11:39:48 +0000

    Update test description

--- tests/settings/test_settings.py
+++ tests/settings/test_settings.py
@@ -51,7 +51,7 @@ async def test_custom(mock_json_loads):
 @patch("lims_utils.settings.json.loads")
 @patch("lims_utils.settings.Path.read_text", new=new_read_all)
 async def test_nested(mock_json_loads):
-    """Should return custom values"""
+    """Should return nested values"""
     mock_json_loads.return_value = {
         "auth": {"endpoint": "https://localhost/diff-auth"},
         "db": {"pool": 90},
